afd1b1443ba7b6668ec82fb222249c0358aaedec
MrWinRock <mrwinrock11@gmail.com>
2025-01-09 23:23:39 +0700

    update: work5

--- Work5_Navigations/expoNew05/App.js
+++ Work5_Navigations/expoNew05/App.js
@@ -1,9 +1,18 @@
-import { View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <></>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
